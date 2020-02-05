8dfe2f9ad51241fcf8dca39a8eb62a19972e700d
crodulfo <crodulfo@HXNB-CRODULFO>
2020-02-05 13:25:21 -0300

    pepe

---
+++ AUSATest/AUSATest/ItemDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUSATest
+{
+    public class ItemDetalle
+    {
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public string Nombre_Estacion { get; set; }
+        public float Importe { get; set; }
+        public int Categoria { get; set; }
+        public string Via { get; set; }
+        public string Dominio { get; set; }
+    }
+}
