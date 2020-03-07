da86f83e4b61a5deb9f9b2337ee80db4d60f6e2e
crodulfo <crodulfo@HXNB-CRODULFO>
2020-03-07 02:02:58 -0300

    Angular 9 y arreglos generales de contexto

--- TeleQuick/TeleQuick/TeleQuick/ClientApp/src/app/services/app-translation.service.ts
+++ TeleQuick/TeleQuick/TeleQuick/ClientApp/src/app/services/app-translation.service.ts
@@ -29,7 +29,7 @@ export class AppTranslationService {
   }
 
   getBrowserLanguage() {
-    return this.translate.getBrowserLang();
+    return this.translate.defaultLang != null ? this.translate.defaultLang : this.translate.getBrowserLang();
   }
 
   getCurrentLanguage() {
