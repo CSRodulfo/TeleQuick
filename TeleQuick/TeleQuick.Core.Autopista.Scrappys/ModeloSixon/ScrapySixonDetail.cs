bd9ef3c76e6e74dc365da138c0dd84be61a1251a
Sebas <csrodulfo@outlook.com>
2020-04-01 20:19:22 -0300

    Scrappy Error de Varios Dominios AUSA

--- TeleQuick/TeleQuick.Core.Autopista.Scrappys/ModeloSixon/ScrapySixonDetail.cs
+++ TeleQuick/TeleQuick.Core.Autopista.Scrappys/ModeloSixon/ScrapySixonDetail.cs
@@ -21,56 +21,57 @@ namespace TeleQuick.AutopistaAUSA
 
         private async Task<List<DetailResponse>> ObtenerDatosDescargados(byte[] PdffileName)
         {
-            bool finBusqueda = false;
-            int ultPos = 0;
-            string linea = String.Empty;
             String[] _array = PDFFile.ReadPdfFile(PdffileName).Split('\n');
-            if (_array.Length <= 14)
-            {
-                // return Task;
-            }
-            linea = _array[14];
-            List<String> detalleItem = new List<string>();
-            string _item = _array[12];
-            int posD = _item.IndexOf("Dominio:") + 8;
-            int posT = _item.IndexOf("TAG");
-            var _strDom = _item.Substring(posD, posT - posD).Trim();
-            var _strTag = _item.Substring(_item.Length - 12, 12).Trim();
-
-            while (!finBusqueda)
-            {
-                int pos = linea.IndexOf(',', ultPos);
-                if (pos > 0)
-                {
-                    detalleItem.Add(linea.Substring(ultPos, (pos + 3 - ultPos)));
-                    ultPos = pos + 3;
-                }
-                if (pos == -1 || ultPos >= linea.Length || linea.IndexOf("*") < 5)
-                {
-                    finBusqueda = true;
-                }
-            }
             List<DetailResponse> list = new List<DetailResponse>();
-            foreach (string it in detalleItem)
+
+            for (int i = 14; i < _array.Length; i += 3)
             {
-                DetailResponse id = new DetailResponse();
-                int lastSpace = it.LastIndexOf(' ');
-                id.Campo6 = it.Substring(lastSpace, it.Length - lastSpace).Trim().Replace(",", ".");
-                string[] detArray = it.Substring(0, lastSpace).Trim().Split(' ');
-                id.Campo0 = detArray[0];
-                id.Campo1 = detArray[1];
-                id.Campo2 = detArray[2];
-                id.Campo3 = detArray[3];
-                id.Campo4 = _strDom;
-                id.Campo7 = _strTag;
-                for (int i = 4; i < detArray.Length; i++)
+                List<String> detalleItem = new List<string>();
+                bool finBusqueda = false;
+                int ultPos = 0;
+                string linea = String.Empty;
+                linea = _array[i];
+
+                string _item = _array[i - 2];
+                int posD = _item.IndexOf("Dominio:") + 8;
+                int posT = _item.IndexOf("TAG");
+                var _strDom = _item.Substring(posD, posT - posD).Trim();
+                var _strTag = _item.Substring(_item.Length - 12, 12).Trim();
+
+                while (!finBusqueda)
                 {
-                    id.Campo5 += detArray[i] + " ";
+                    int pos = linea.IndexOf(',', ultPos);
+                    if (pos > 0)
+                    {
+                        detalleItem.Add(linea.Substring(ultPos, (pos + 3 - ultPos)));
+                        ultPos = pos + 3;
+                    }
+                    if (pos == -1 || ultPos >= linea.Length || linea.IndexOf("*") < 5)
+                    {
+                        finBusqueda = true;
+                    }
                 }
 
-                list.Add(id);
-            }
+                foreach (string it in detalleItem)
+                {
+                    DetailResponse id = new DetailResponse();
+                    int lastSpace = it.LastIndexOf(' ');
+                    id.Campo6 = it.Substring(lastSpace, it.Length - lastSpace).Trim().Replace(",", ".");
+                    string[] detArray = it.Substring(0, lastSpace).Trim().Split(' ');
+                    id.Campo0 = detArray[0];
+                    id.Campo1 = detArray[1];
+                    id.Campo2 = detArray[2];
+                    id.Campo3 = detArray[3];
+                    id.Campo4 = _strDom;
+                    id.Campo7 = _strTag;
+                    for (int l = 4; l < detArray.Length; l++)
+                    {
+                        id.Campo5 += detArray[l] + " ";
+                    }
 
+                    list.Add(id);
+                }
+            }
             return list;
         }
     }
